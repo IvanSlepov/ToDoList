b2ccc31bc179ba9da635fc777de9fc032273d048
Ivan Sliepov <slepow1985@gmail.com>
2020-08-31 15:56:39 +0300

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Ivan Sliepov on 31.08.2020.
+//  Copyright © 2020 Ivan Sliepov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
